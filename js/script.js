d87d3d00c526d7f0dc1f9513609bfb7f47a19d1b
felipeacelino <felipeacelino@hotmail.com>
2017-01-07 12:53:35 -0200

    Update

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@
 				front.setAttribute('class', 'front');
 
 				var imgFront = doc.createElement('img');
-				imgFront.setAttribute('src', 'images/question.jpg');
+				imgFront.setAttribute('src', 'images/default.jpg');
 				front.appendChild(imgFront);
 				card.appendChild(front);
 
@@ -262,7 +262,7 @@
 	}
 
 	var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
-	//var images = ['1.jpg','2.jpg','3.jpg'];
+	//var images = ['1.jpg'];
 	
 	var game = new Game(images);
 	
